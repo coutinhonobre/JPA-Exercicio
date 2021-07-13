486269cea97868b87b8c5d93bd294d203fae6660
Igor Coutinho Ferreira Nobre <coutinho@Igors-MacBook-Pro.local>
2021-07-13 00:42:07 -0300

    first commit

---
+++ src/main/java/exerciciojpa/application/Application.java
@@ -0,0 +1,26 @@
+package exerciciojpa.application;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import exerciciojpa.entities.Marca;
+
+public class Application {
+
+	public static void main(String[] args) {
+System.out.println("\n*** Versão 1 - Inicial ***");
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exerciciojpa");
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		
+		Marca marca = new Marca("BMW");
+		
+		
+		em.persist(marca);
+		em.getTransaction().commit();
+	}
+
+}
